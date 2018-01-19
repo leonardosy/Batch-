d4b4521b0eb8e0585c1a51a07762cebb4b5eb9b7
Leonardo Oliveira F Silva <scud.leonardo@gmail.com>
2018-01-18 23:37:56 -0200

    Create Correcao SAP.bat

---
+++ Correcao SAP.bat
@@ -0,0 +1,32 @@
+@echo off
+echo ================================================
+echo                   CORRECAO SAP
+echo ================================================
+pause
+
+@echo on
+
+REM Copia os Arquivos (Inseri caminho na rede)
+copy D:\scud-leonardo\Desktop\bat\1.txt C:\Windows\System32
+REM copy \\sjkfs13\Backhelp\
+CACLS 1.txt /E /G "scud-teste":F
+copy D:\scud-leonardo\Desktop\bat\1.txt C:\Windows\SysWOW64
+cd ..
+cd SysWOW64
+CACLS 1.txt /E /G "scud-teste":F
+
+REM Define Variavel do Sistema
+SETX SNC_LIB "C:\Windows\System32" /M
+
+REM C:
+REM cd Windows
+REM cd System32a
+REM cd scud-leonardo
+REM cd Desktop
+REM cd teste
+REM CACLS 1.txt /E /G "scud-teste":F
+REM CACLS 2.txt /E /G "scud-teste":F
+REM CACLS 3.txt /E /G "scud-teste":F
+REM SET /P input=
+REM echo SAIDA: %input%
+pause
